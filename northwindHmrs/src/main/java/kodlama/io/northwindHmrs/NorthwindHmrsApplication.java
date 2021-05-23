27d386ecea1f0ad80fcda2f56a44c63213fe3b5f
mhmtaysal <mhmtaysal@gmail.com>
2021-05-23 22:38:56 +0300

    swagger added

--- northwindHmrs/src/main/java/kodlama/io/northwindHmrs/NorthwindHmrsApplication.java
+++ northwindHmrs/src/main/java/kodlama/io/northwindHmrs/NorthwindHmrsApplication.java
@@ -2,7 +2,14 @@ package kodlama.io.northwindHmrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
 public class NorthwindHmrsApplication {
 
@@ -10,4 +17,13 @@ public class NorthwindHmrsApplication {
 		SpringApplication.run(NorthwindHmrsApplication.class, args);
 	}
 
+	
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("kodlama.io.northwindHmrs"))              
+          .build();                                           
+    }
 }
